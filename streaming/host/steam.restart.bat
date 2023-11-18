a1dcb90e82e9c7cac7437831338fdd1d077d3b7b
Spuxx <39671641+spuxx1701@users.noreply.github.com>
2023-11-18 14:17:40 +0100

    Update steam.restart.bat

--- streaming/host/steam.restart.bat
+++ streaming/host/steam.restart.bat
@@ -3,9 +3,12 @@ ECHO Shutting down Steam...
 
 "C:\Program Files (x86)\Steam\steam.exe" -shutdown
 
-ECHO Steam will be restarted in 15 seconds.
-TIMEOUT 15
+set timeout=15
+ECHO Steam will be restarted in %timeout% seconds.
+TIMEOUT %timeout%
 
-ECHO Restarting Steam.
-CALL "C:\Program Files (x86)\Steam\Steam.exe"
+ECHO Restarting Steam...
+START "Steam" "C:\Program Files (x86)\Steam\Steam.exe" /b
+ECHO Steam has been restarted. This terminal will be closed.
+TIMEOUT 5
 EXIT 0
